b0ca1f952ba4f6c1db8a7909ef662a909a6504bf
Nikhil Das Karavatt <121696401+NDK22@users.noreply.github.com>
2023-09-25 20:42:18 -0500

    Update Solution.py

--- Leet75 / reverse-words-in-a-string /Solution.py
+++ Leet75 / reverse-words-in-a-string /Solution.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        each_word = s.split()
+        reversed_words= reversed(each_word)
+        reversed_string = ' '.join(reversed_words)
+        return reversed_string
